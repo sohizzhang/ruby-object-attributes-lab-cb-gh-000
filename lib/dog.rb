c8c896eb57d90f0e81e4b41026ffe45622054c09
S Yoon <sohizzhang@gmail.com>
2019-06-06 14:00:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
-  def name
-    @name = name
+  def name=(dog_name)
+    @name = dog_name
   end
 
   def name
     @name
   end
 
-  def breed
-    @breed = breed
+  def breed=(dog_breed)
+    @breed = dog_breed
   end
 
   def breed
